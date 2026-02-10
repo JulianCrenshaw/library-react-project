d09d24e5dccfba4926df962613230d8ef591eb42
Julian <julian.gold.crenshaw@gmail.com>
2026-02-10 06:33:08 -0600

    menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-   <nav>
-    <div className="nav__container">
+    <nav>
+      <div className="nav__container">
         <a href="/">
-        <img src="" alt="" />
+          <img src="" alt="" className="logo" />
         </a>
-    </div>
-   </nav>
-  )
-}
+        <ul className="nav__links">
+          <li className="nav__list">
+            <a href="" className="navlink">
+              Home
+            </a>
+          </li>
+          <li className="nav__list">
+            <a href="" className="navlink">
+              Books
+            </a>
+          </li>
+          <button className="btn__menu"></button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
